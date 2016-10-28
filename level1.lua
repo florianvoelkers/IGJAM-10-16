9d73f782b4f219fb3f939e8a311c29294d2e5194
florianvoelkers <florian.voelkers@mail.de>
2016-10-28 21:14:59 +0200

    rudimentärer start-screen mit menü und erstes level, mit escape kommt man aus dem level

---
+++ level1.lua
@@ -0,0 +1,112 @@
+-----------------------------------------------------------------------------------------
+--
+-- level1.lua
+--
+-----------------------------------------------------------------------------------------
+
+local composer = require( "composer" )
+local scene = composer.newScene()
+
+-- include Corona's "physics" library
+local physics = require "physics"
+
+--------------------------------------------
+
+-- forward declarations and other locals
+local screenW, screenH, halfW = display.actualContentWidth, display.actualContentHeight, display.contentCenterX
+
+local function onKeyEvent (event)
+	print("escape")
+	if event.keyName == "escape" then
+		composer.gotoScene( "menu" )
+		Runtime:removeEventListener( "key", onKeyEvent )
+		composer.removeHidden() 
+		composer.removeScene( "level1" )
+	end
+end
+
+function scene:create( event )
+
+	-- Called when the scene's view does not exist.
+	-- 
+	-- INSERT code here to initialize the scene
+	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
+
+	local sceneGroup = self.view
+
+	-- We need physics started to add bodies, but we don't want the simulaton
+	-- running until the scene is on the screen.
+	physics.start()
+	physics.pause()
+
+
+	-- create a grey rectangle as the backdrop
+	-- the physical screen will likely be a different shape than our defined content area
+	-- since we are going to position the background from it's top, left corner, draw the
+	-- background at the real top, left corner.
+	local background = display.newRect( display.screenOriginX, display.screenOriginY, screenW, screenH )
+	background.anchorX = 0 
+	background.anchorY = 0
+	background:setFillColor( .5 )
+	
+	-- all display objects must be inserted into group
+	sceneGroup:insert( background )
+
+	Runtime:addEventListener( "key", onKeyEvent )
+end
+
+
+function scene:show( event )
+	local sceneGroup = self.view
+	local phase = event.phase
+	
+	if phase == "will" then
+		-- Called when the scene is still off screen and is about to move on screen
+	elseif phase == "did" then
+		-- Called when the scene is now on screen
+		-- 
+		-- INSERT code here to make the scene come alive
+		-- e.g. start timers, begin animation, play audio, etc.
+		physics.start()
+	end
+end
+
+function scene:hide( event )
+	local sceneGroup = self.view
+	Runtime:removeEventListener( "key", onKeyEvent )
+	local phase = event.phase
+	
+	if event.phase == "will" then
+		-- Called when the scene is on screen and is about to move off screen
+		--
+		-- INSERT code here to pause the scene
+		-- e.g. stop timers, stop animation, unload sounds, etc.)
+		physics.stop()
+	elseif phase == "did" then
+		-- Called when the scene is now off screen
+	end	
+	
+end
+
+function scene:destroy( event )
+	-- Called prior to the removal of scene's "view" (sceneGroup)
+	-- 
+	-- INSERT code here to cleanup the scene
+	-- e.g. remove display objects, remove touch listeners, save state, etc.
+	local sceneGroup = self.view
+	
+	--package.loaded[physics] = nil
+	--physics = nil
+end
+
+---------------------------------------------------------------------------------
+
+-- Listener setup
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+-----------------------------------------------------------------------------------------
+
+return scene
