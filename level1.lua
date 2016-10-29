c1e158a2b302d47d2429ac18b1a7cfb63884361c
florianvoelkers <florian.voelkers@mail.de>
2016-10-29 22:42:48 +0200

    play button full functional

--- level1.lua
+++ level1.lua
@@ -153,6 +153,7 @@ local function onKeyEvent (event)
 			pressedTimer = 0
 		end
 	elseif event.keyName == "escape" then
+		physics.stop( )
 		composer.gotoScene( "end" )
 		Runtime:removeEventListener( "key", onKeyEvent )
 		composer.removeHidden() 
@@ -543,11 +544,6 @@ function scene:create( event )
 	sceneGroup = self.view
 
 	display.setDefault("isAnchorClamped",false)
-	
-
-	physics.start()
-	physics.setGravity( 0, 0)
-	physics.setDrawMode( "normal" )
 
 	local background = display.newImageRect( "assets/map/background.png", display.contentWidth, display.contentHeight )
 
@@ -559,6 +555,12 @@ function scene:create( event )
 	Runtime:addEventListener( "key", onKeyEvent )
 
 	local worldGroup = display.newGroup()
+
+	print("physics")
+
+	physics.start()
+	physics.setGravity( 0, 0)
+	physics.setDrawMode( "normal" )
 
 	earth2 = display.newImageRect (worldGroup, "assets/map/earth.png", 300, 300 )
 	earth2.x, earth2.y = display.contentCenterX, display.contentCenterY
@@ -655,13 +657,14 @@ end
 
 
 function scene:show( event )
+	print("show")
 	local sceneGroup = self.view
 	local phase = event.phase
 	
 	if phase == "will" then
+		print("will")
 	elseif phase == "did" then
-
-		physics.start()
+		print("did")
 	end
 end
 
--- menu.lua
+++ menu.lua
@@ -10,58 +10,231 @@ local scene = composer.newScene()
 -- include Corona's "widget" library
 local widget = require "widget"
 
+local world
+local moon
+local playButton
+local highscoreButton
+local leftSide
+local rightSide
+local middleBar
+local particleSystem
+local left
+local right
+local speed
+local maxspeed
+local leftTouchArea
+local rightTouchArea
+
+local sceneNotCalled = true
+
 --------------------------------------------
 
--- forward declarations and other locals
-local playBtn
+local function onTouchLeft(event)
+	if event.phase == "began" then
+		left = true
+		right = false
+	elseif event.phase == "ended" then
+		left = false
+		right = false
+	end
+end
+local function onTouchRight(event)
+	if event.phase == "began" then
+		left = false
+		right = true
+	elseif event.phase == "ended" then
+		left = false
+		right = false
+	end
+end
 
--- 'onRelease' event listener for playBtn
-local function onPlayBtnRelease()
-	
-	-- go to level1.lua scene
-	composer.gotoScene( "level1", "fade", 500 )
-	
-	return true	-- indicates successful touch
+local function onFrame(event)
+	if 	left == false and right == false and (speed > 0 or speed < 0) then
+		if speed >0.01 then
+			speed = speed -0.005
+		elseif speed < - 0.01 then
+			speed = speed +0.005
+		else
+			speed = 0
+		end
+	elseif left == true and right == false and speed < maxspeed then
+		speed = speed + 0.008
+	elseif left == false and right == true and speed > -maxspeed then
+		speed = speed - 0.008
+	elseif left == true or right == true then
+		speed = speed
+	end
+
+	leftSide.rotation = leftSide.rotation + speed
+	rightSide.rotation = rightSide.rotation + speed
+	middleBar.rotation = middleBar.rotation + speed
+	if leftSide.rotation < -225 then
+		middleBar.rotation = 0
+		leftSide.rotation = 135
+		rightSide.rotation = -135
+	elseif leftSide.rotation > 135 then
+		middleBar.rotation = 0
+		leftSide.rotation = -224.9
+		rightSide.rotation = -494.9
+	end
+	moon.rotation = moon.rotation +speed
+
+	local gravityY 
+	if leftSide.rotation <= 135 and leftSide.rotation >= -45 then
+		gravityY = -1 * (leftSide.rotation - 45) / 9
+	else
+		gravityY = (leftSide.rotation + 135) / 9
+	end
+	local gravityX
+	if leftSide.rotation <= 135 and leftSide.rotation >= 45 then
+		gravityX = (leftSide.rotation - 135) / 9
+	elseif leftSide.rotation < 45 and leftSide.rotation >= -135 then
+		gravityX = -1 * (leftSide.rotation + 45) / 9
+	else
+		gravityX = (leftSide.rotation + 225) / 9
+	end
+
+	physics.setGravity(gravityX, gravityY)
+
+	gravityY = nil
+	gravityX = nil
+
+
+	playButton.hit = particleSystem:rayCast( 0, display.contentCenterY ,display.contentCenterX, display.contentCenterY)
+	if playButton.hit then
+		playButton.hp = playButton.hp - 1
+		playButton:setFillColor( playButton.hp/100, playButton.hp/100,  playButton.hp/100)
+		if playButton.hp == 0 then
+			Runtime:removeEventListener( "enterFrame", onFrame )
+			physics.setGravity(0, 0)
+			display.remove(particleSystem)
+			particleSystem = nil
+			physics.stop()
+			if sceneNotCalled then
+				sceneNotCalled = false
+				playButton:removeSelf( )
+				leftTouchArea.isHitTestable = false
+				rightTouchArea.isHitTestable = false
+				timer.performWithDelay( 500, function (...)
+					composer.gotoScene( "level1", "fade", 500)
+				end )
+			end
+		end
+	end
+
 end
 
 function scene:create( event )
 	local sceneGroup = self.view
 
-	-- Called when the scene's view does not exist.
-	-- 
-	-- INSERT code here to initialize the scene
-	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
+	left = false
+	right = false
+	speed = 0
+	maxspeed = 0.5
 
-	-- display a background image
-	-- print(display.actualContentWidth)
-	-- print(display.contentWidth)
-	-- print(display.contentHeight)
-	-- print(display.actualContentHeight)
-	local background = display.newImageRect( "assets/concept_size.png", display.contentWidth, display.contentHeight )
+	display.setDefault("isAnchorClamped",false)
+
+	physics.start()
+	physics.setGravity( 0, 0)
+	physics.setDrawMode( "normal" )
+
+	local background = display.newImageRect( "assets/map/background.png", display.contentWidth, display.contentHeight )
 	background.x = display.contentCenterX
 	background.y = display.contentCenterY
+
+	world = display.newImageRect("assets/map/earth.png", 346, 346 )
+	world.x = display.contentCenterX
+	world.y = display.contentCenterY
+	world.myName = "world"
+	physics.addBody( world, "static",{radius=178}  )
+
+	moon = display.newImageRect( "assets/map/moon.png", 123, 123 )
+	moon.x,moon.y = world.x,world.y
+	moon.anchorX = 0.5
+	moon.anchorY = -2.3
 	
 	-- create/position logo/title image on upper-half of the screen
-	local titleLogo = display.newImageRect( "logo.png", 346, 55 )
-	titleLogo.x = display.contentCenterX + 18
-	titleLogo.y = display.contentCenterY
-	
-	-- create a widget button (which will loads level1.lua on release)
-	playBtn = widget.newButton{
-		label="Play Now",
-		labelColor = { default={255}, over={128} },
-		defaultFile="button.png",
-		overFile="button-over.png",
-		width=154, height=40,
-		onRelease = onPlayBtnRelease	-- event listener function
+	local titleLogo = display.newImageRect( "logo.png", 692, 110 )
+	titleLogo.x = display.contentCenterX
+	titleLogo.y = display.contentCenterY - world.height * 0.5 - 55
+
+	playButton = display.newImageRect( "assets/play.png", 100, 100 )
+	playButton.x, playButton.y = display.contentCenterX - world.width * 0.5 - 25, display.contentCenterY
+	playButton.hp = 100
+
+	leftSide = display.newRect( world.x,world.y, 600, 90 )
+	leftSide.alpha=0
+	leftSide.rotation = 135
+	leftSide.anchorX = 0.6
+	leftSide.anchorY = 2.8
+	leftSide.myName = "leftSide"
+
+	physics.addBody( leftSide, "static" )
+
+	rightSide = display.newRect( world.x,world.y, 600, 90 )
+	rightSide.alpha=0
+	rightSide.rotation = -135
+	rightSide.anchorX = 0.4
+	rightSide.anchorY = 2.8
+	rightSide.myName = "rightSide"
+
+	physics.addBody( rightSide, "static" )
+
+
+	middleBar = display.newRect(world.x,world.y,600,90)
+	middleBar.alpha=0
+	middleBar.rotation = 0
+	middleBar.anchorX = 0.5
+	middleBar.anchorY = -0.9
+	middleBar.myName = "middleBar"
+
+	physics.addBody( middleBar, "static" )
+
+	particleSystem = physics.newParticleSystem{
+		filename = "assets/liquidParticle.png",
+		radius = 3,
+		imageRadius = 5,
+		gravityScale = 1.0,
+		strictContactCheck = true
 	}
-	playBtn.x = display.contentCenterX + 18
-	playBtn.y = display.contentHeight - 125
-	
+	particleSystem:createGroup(
+	    {
+	        flags = { "water" , "fixtureContactListener"},
+	        x = world.x,
+	        y = world.y+world.height/2 + 4,
+	        halfWidth = 18,
+	        halfHeight = 18
+	   	    }
+	)
+	particleSystem.myName = "Wasser"
+
+	Runtime:addEventListener( "enterFrame", onFrame )
+
+	leftTouchArea = display.newRect( 0, 0, display.actualContentWidth * 0.5, display.actualContentHeight )
+	leftTouchArea.anchorX, leftTouchArea.anchorY = 0, 0
+	leftTouchArea.isVisible = false
+	leftTouchArea.isHitTestable = true
+	leftTouchArea:setFillColor( 1, 1, 1 )
+	leftTouchArea:addEventListener( "touch", onTouchLeft )
+
+	rightTouchArea = display.newRect( display.actualContentWidth, 0, display.actualContentWidth * 0.5, display.actualContentHeight )
+	rightTouchArea.anchorX, rightTouchArea.anchorY = 1, 0
+	rightTouchArea.isVisible = false
+	rightTouchArea.isHitTestable = true
+	rightTouchArea:setFillColor( 1, 1, 1 )
+	rightTouchArea:addEventListener( "touch", onTouchRight )
+
 	-- all display objects must be inserted into group
 	sceneGroup:insert( background )
 	sceneGroup:insert( titleLogo )
-	sceneGroup:insert( playBtn )
+	sceneGroup:insert( world )
+	sceneGroup:insert( playButton)
+	sceneGroup:insert( moon )
+	sceneGroup:insert( leftSide )
+	sceneGroup:insert( rightSide )
+	sceneGroup:insert( middleBar )
+	
+	
 end
 
 function scene:show( event )
@@ -71,10 +244,7 @@ function scene:show( event )
 	if phase == "will" then
 		-- Called when the scene is still off screen and is about to move on screen
 	elseif phase == "did" then
-		-- Called when the scene is now on screen
-		-- 
-		-- INSERT code here to make the scene come alive
-		-- e.g. start timers, begin animation, play audio, etc.
+		physics.start()
 	end	
 end
 
@@ -83,10 +253,7 @@ function scene:hide( event )
 	local phase = event.phase
 	
 	if event.phase == "will" then
-		-- Called when the scene is on screen and is about to move off screen
-		--
-		-- INSERT code here to pause the scene
-		-- e.g. stop timers, stop animation, unload sounds, etc.)
+		physics.stop()
 	elseif phase == "did" then
 		-- Called when the scene is now off screen
 	end	
