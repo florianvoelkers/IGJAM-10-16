4f4fde9669d65cf1e180121dbddb63a0a3851fc3
Nikolai_Laptop <Nikolai.stecker@gmx.de>
2016-10-30 01:27:16 +0200

    hide android buttons

--- main.lua
+++ main.lua
@@ -6,6 +6,7 @@
 
 -- hide the status bar
 display.setStatusBar( display.HiddenStatusBar )
+native.setProperty( "androidSystemUiVisibility", "immersiveSticky" )
 
 -- include the Corona "composer" module
 local composer = require "composer"
